8509ae9493af51334659537ef8e6746b6eadce31
maxdavila <adavila0711@gmail.com>
2013-12-01 23:15:20 -0800

    solution to timeserver

--- learnyounode/timeserver.js
+++ learnyounode/timeserver.js
@@ -1,1 +1,18 @@
 var net = require('net');
+var server = net.createServer(function(socket){
+  socket.end( getFormattedDate() + '\n');
+
+});
+server.listen(8000);
+function getFormattedDate() {
+  var date = new Date();
+  return date.getFullYear() + '-' +
+         addPadding(getMonth() + 1) + '-' +
+         addPadding(date.getDate()) + ' ' +
+         addPadding(date.getHours()) + ':' +
+         addPadding(date.getMinutes());
+}
+
+function addPadding(num) {
+  return (num < 10 ? '0' + num : num);
+}
