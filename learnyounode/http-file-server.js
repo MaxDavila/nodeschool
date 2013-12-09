acbdff1185d5d87c2ccdaa8a76df0beb39880a0c
maxdavila <adavila0711@gmail.com>
2013-12-08 21:21:50 -0800

    finish http file server exercise

--- learnyounode/http-file-server.js
+++ learnyounode/http-file-server.js
@@ -1,4 +1,8 @@
 var http = require('http');
-var server = http.createServer(function (req, res) {
+var fs = require('fs');
+var file = fs.createReadStream(process.argv[2]);
 
+var server = http.createServer(function (req, res){
+  file.pipe(res);
 });
+server.listen(8000);
