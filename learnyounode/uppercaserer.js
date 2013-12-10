f633eba3cf7a6adc967f25b3ffdcc07616fb3ef7
maxdavila <adavila0711@gmail.com>
2013-12-09 21:55:14 -0800

    add solution to uppercaserer

--- learnyounode/uppercaserer.js
+++ learnyounode/uppercaserer.js
@@ -1,1 +1,13 @@
 var map = require('through2-map');
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+  if (req.method != 'POST') return res.end('You nned to send a POST\n');
+
+  req.pipe(map(function (chunk){
+    return chunk.toString().toUpperCase();
+  })).pipe(res);
+});
+
+server.listen(8000);
+
