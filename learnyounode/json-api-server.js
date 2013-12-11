c48e20f5aa8851a8cc230606f3eaf34f85743999
maxdavila <adavila0711@gmail.com>
2013-12-11 00:16:51 -0800

    refactored parseTime function outside the server

--- learnyounode/json-api-server.js
+++ learnyounode/json-api-server.js
@@ -3,34 +3,42 @@ var url = require('url');
 
 var server = http.createServer(function(req, res){
   if (req.method != 'GET') return res.end("Can't handle request different than GET sawry");
-  var path = url.parse(req.url, true).pathname;
-  var query = url.parse(req.url, true).query;
-  var date = new Date();
+  var path = url.parse(req.url, true).pathname,
+      query = url.parse(req.url, true).query,
+      date = new Date(),
+      json;
 
   switch(path) {
     case '/api/parsetime':
       res.writeHead(200, {'Content-Type': 'application/json' });
-      var json = {
-        "hour" : date.getHours(),
-        "minute" : date.getMinutes(),
-        "second" : date.getSeconds()
-      };
-      res.write(JSON.stringify(json));
+      res.write(JSON.stringify( parseTime(date) ));
       break;
     case '/api/unixtime':
       res.writeHead(200, {'Content-Type': 'application/json' });
-      var json = {
-        "unixtime" : date.getTime()
-      };
-      res.write(JSON.stringify(json));
+      res.write(JSON.stringify( parseUnixTime(date) ));
       break;
     default:
       res.writeHead(404, {'Content-Type': 'text/html' });
       res.write("The page that you are looking for does not exist");
       break;
   }
-      res.end();
+  res.end();
+
+
 
 });
 
+function parseTime(date) {
+  return {
+        hour : date.getHours(),
+        minute : date.getMinutes(),
+        second : date.getSeconds()
+  };
+}
+
+function parseUnixTime(date) {
+  return {
+        unixtime : date.getTime()
+  };
+}
 server.listen(8000);
