42d6ce93d913d1c9e50e3781f4f7edab0d8de818
maxdavila <adavila0711@gmail.com>
2013-12-10 23:55:00 -0800

    finish json-api-server

--- learnyounode/json-api-server.js
+++ learnyounode/json-api-server.js
@@ -5,15 +5,24 @@ var server = http.createServer(function(req, res){
   if (req.method != 'GET') return res.end("Can't handle request different than GET sawry");
   var path = url.parse(req.url, true).pathname;
   var query = url.parse(req.url, true).query;
+  var date = new Date();
 
   switch(path) {
     case '/api/parsetime':
       res.writeHead(200, {'Content-Type': 'application/json' });
-      res.write('yo parsetime yo: ' + query.iso);
+      var json = {
+        "hour" : date.getHours(),
+        "minute" : date.getMinutes(),
+        "second" : date.getSeconds()
+      };
+      res.write(JSON.stringify(json));
       break;
     case '/api/unixtime':
       res.writeHead(200, {'Content-Type': 'application/json' });
-      res.write('yo epoch time: ' + query.iso);
+      var json = {
+        "unixtime" : date.getTime()
+      };
+      res.write(JSON.stringify(json));
       break;
     default:
       res.writeHead(404, {'Content-Type': 'text/html' });
