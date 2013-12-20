f5211729d8a4c81d4baf8da5de1c2782a909fb46
maxdavila <adavila0711@gmail.com>
2013-12-19 19:55:57 -0500

    changed to write fn, failing test

--- stream-adventure/transform.js
+++ stream-adventure/transform.js
@@ -1,6 +1,7 @@
 var map = require('through');
 var tr = map(write, end);
-process.stdin.pipe(tr);
+tr.write('yo\n');
+tr.end();
 
 function write(buf) { console.log(this); }
 function end() { console.log('__END__');}
