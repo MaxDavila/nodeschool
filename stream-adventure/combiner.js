e0f15b0ac8ce4f1eaf7e4c0cdc0b4ec324f20753
maxdavila <adavila0711@gmail.com>
2014-01-06 22:43:37 -0500

    add module export fn

--- stream-adventure/combiner.js
+++ stream-adventure/combiner.js
@@ -1,1 +1,8 @@
 var combine = require('stream-combiner')
+module.exports = function () {
+    return combine(
+        // read newline-separated json,
+        // group books into genres,
+        // then gzip the output
+    )
+}
