eccd5ced7fc6240bf3aeb94b08ec3ac97075ed7f
maxdavila <adavila0711@gmail.com>
2014-01-07 23:46:22 -0800

    add module export fn

--- stream-adventure/duplexer.js
+++ stream-adventure/duplexer.js
@@ -1,1 +1,5 @@
 var spawn = require('child_process').spawn;
+module.exports = function (cmd, args) {
+      // spawn the process and return a single stream
+      // joining together the stdin and stdout here
+  };
