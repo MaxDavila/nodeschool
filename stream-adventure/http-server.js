70d5049792a3cff874212fe3892f6c05719efe8d
maxdavila <adavila0711@gmail.com>
2014-01-08 23:19:18 -0800

    add server

--- stream-adventure/http-server.js
+++ stream-adventure/http-server.js
@@ -1,2 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var server = http.createServer(function(req, res) {
+	fs.createReadStream('file.txt').pipe(res);
+});
+server.listen(process.argv.slice(2));
