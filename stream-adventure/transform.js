4711aa7a2d085c8d25484f0ecc135320c360d3e1
maxdavila <adavila0711@gmail.com>
2014-01-12 23:51:31 -0800

    add write fn

--- stream-adventure/transform.js
+++ stream-adventure/transform.js
@@ -1,6 +1,8 @@
 var map = require('through');
 var tr = map(write, end);
-
+tr.write('beep\n')
+tr.write('boop\n')
+tr.end();
 
 function write(buf) { console.log(this); }
 function end() { console.log('__END__');}
