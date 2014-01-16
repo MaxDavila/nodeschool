e8713a5e918c1f04fbe19c7e3576202eebe11c92
maxdavila <adavila0711@gmail.com>
2014-01-15 23:27:36 -0800

    add sol for ex5

---
+++ functional-javascript/ex5.js
@@ -0,0 +1,11 @@
+module.exports = function checkUsersValid(goodUsers){
+	
+	return function(submittedUsers){
+		return submittedUsers.every(function(subUser){
+			
+		  return goodUsers.some(function(goodUser){
+				return (goodUser.id === subUser.id);
+			});
+		});
+	};
+}
