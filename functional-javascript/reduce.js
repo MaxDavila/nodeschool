b98f335c1526f1939c113a3899047dabc1709f1f
maxdavila <adavila0711@gmail.com>
2014-01-16 18:34:30 -0800

    refactored method

--- functional-javascript/reduce.js
+++ functional-javascript/reduce.js
@@ -1,16 +1,9 @@
 function countWords(inputWords) {
-	var result = {};
-	inputWords.reduce(function(prev, curr){
-		if (result[curr] === undefined) {
-			return result[curr] = 1;
-		} else {
-			return result[curr]++;
-		}
-	});
-	return result;
+	return inputWords.reduce(function(result, curr){
+		result[curr] = ++result[curr] || 1;
+		return result
+	}, {});
 
 }
-// var inputWords = ['Apple', 'Banana', 'Apple', 'Durian', 'Durian', 'Durian']
 
-// var a = console.log(countWords(inputWords))
 module.exports = countWords;
