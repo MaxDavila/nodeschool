f8d23f87853dc83e055caa1957be3cdd845505ee
maxdavila <adavila0711@gmail.com>
2014-01-19 12:49:45 -0800

    playing with moar prototypes

--- functional-javascript/basic-call.js
+++ functional-javascript/basic-call.js
@@ -1,5 +1,9 @@
 function duckCount(args){
 	console.log(args);
+	var object = Object.create(null)
+  object.quack = function() {
+  console.log('quack')
+}
 	Object.getPrototypeOf(object) === Object.prototype
 	object.hasOwnProperty('quack') 
 }
