0ec356bd4255de6c7fdb0d7c263ecc8b94a56ad4
maxdavila <adavila0711@gmail.com>
2014-01-22 12:34:10 -0800

    use an arbitrary number to do settimeout instead of every call

--- functional-javascript/blocking-event-loop.js
+++ functional-javascript/blocking-event-loop.js
@@ -1,10 +1,15 @@
 function repeat(operation, num) {
 	if(num <= 0) return;
-
-	setInterval(function(){
-		operation();
+	// release control every 10 iterations
+	// 10 is arbitrary
+	operation();
+	if (num % 10 === 0) {
+		setTimeout(function(){
+			repeat(operation, --num);
+		});
+	} else {
 		repeat(operation, --num);
-	});
+	}
 }
 
 module.exports = repeat;
