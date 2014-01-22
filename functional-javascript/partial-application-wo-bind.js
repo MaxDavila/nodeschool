dbcc0e4542f14566d62393addfa740672859a322
maxdavila <adavila0711@gmail.com>
2014-01-21 16:13:57 -0800

    solution to partial application

--- functional-javascript/partial-application-wo-bind.js
+++ functional-javascript/partial-application-wo-bind.js
@@ -2,6 +2,12 @@ var slice = Array.prototype.slice
 
 function logger(namespace){
 
+
+	return function(){
+		var args = slice.call(arguments)
+		args.unshift(namespace)
+		console.log.apply(null, args)
+	}
 }
 
 module.exports = logger
