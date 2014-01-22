b5624a80180ad942d0e5ab255a17274622fedd0e
maxdavila <adavila0711@gmail.com>
2014-01-22 11:45:42 -0800

    refactored solution

--- functional-javascript/map-with-reduce.js
+++ functional-javascript/map-with-reduce.js
@@ -1,6 +1,5 @@
 module.exports = function arrayMap(arr, fn) {
 	return arr.reduce(function(prev, curr, indx, array){
-		prev.push(fn.call(null, curr))
-		return prev
+		return prev.concat(fn(curr));
 	}, [])
 }
