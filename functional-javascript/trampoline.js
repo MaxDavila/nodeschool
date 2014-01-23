7bd31ee5a0889a10e4946d114c6712bad09ee053
maxdavila <adavila0711@gmail.com>
2014-01-22 17:10:32 -0800

    started trampoline

---
+++ functional-javascript/trampoline.js
@@ -0,0 +1,13 @@
+function repeat(operation, num){
+	if (num <= 0) return;
+	operation();
+	return repeat(operation, --num);
+}
+
+function trampoline(fn) {
+
+}
+
+module.exports = function(operation, num) {
+	return repeat(operation, num);
+}
