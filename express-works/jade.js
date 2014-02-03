93e656cdb0a02e7caf7d81dd3190d500f7242895
maxdavila <adavila0711@gmail.com>
2014-02-02 21:58:23 -0800

    change set fn

--- express-works/jade.js
+++ express-works/jade.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('view engine', 'jade')
+app.set('views', path.join(__dirname, 'templates'))
 app.get('/home', function(req, res){
 	res.render('index', {date: new Date().toDateString()} )
 });
