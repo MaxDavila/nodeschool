156f5e94c5f69eb3b3e48871accf99f3ec099034
maxdavila <adavila0711@gmail.com>
2014-02-03 23:50:29 -0800

    add trampoline fn

--- functional-javascript/trampoline.js
+++ functional-javascript/trampoline.js
@@ -1,14 +1,20 @@
-function repeat(operation, num){
-	if (num <= 0) return;
-	operation();
-	return repeat(operation, --num);
+function repeat(operation, num) {
+  if (num <= 0) return;
+  return function() {
+    operation();
+    return repeat(operation, --num);
+  }
 }
 
 function trampoline(fn) {
 
+  while (fn && typeof fn === 'function') {
+    fn = fn();
+  }
+
 }
 
 module.exports = function(operation, num) {
-	return repeat(operation, num);
+	return trampoline(repeat(operation, num));
 }
 
